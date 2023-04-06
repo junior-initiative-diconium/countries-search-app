1b194fbaf663fb44d7436072813408375417d3d4
Katrin Hofstetter <katrin.hofstetter@diconium.com>
2023-04-06 09:23:00 +0200

    initial commit of country app up and running

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
-# Getting Started with Create React App
+# Country API Frontend
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+In this little React app, you can search and filter through a public countries API, that gives you all sorts of information about a country, like population, flag, continent, currency, language and even time zones. I decided to only show the country's name, population, capital and region, but one can always expand the list of information grapped from the API at a later point.
+
+I made this small project with the help of a [tutorial](https://www.freecodecamp.org/news/search-and-filter-component-in-reactjs/). The tutorial gave good information but it was a bit outdated, so the API was altered since the tutorial was created. That's why I had to research a little bit, but it wasn't hard to find the new functioning endpoints of the Country API. This challenge is also on Frontend Mentor and I would like to do more from now on to improve my frontend skills.
 
 ## Available Scripts
 
-In the project directory, you can run:
+In the project directory's root, you can run various scripts:
 
 ### `npm start`
 
@@ -29,42 +31,3 @@ Your app is ready to be deployed!
 
 See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
 
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
-
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
-
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
--- src/App.js
+++ src/App.js
@@ -1,25 +1,121 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss";
+import { useEffect, useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [error, setError] = useState(null);
+	const [isLoaded, setIsLoaded] = useState(false);
+	const [items, setItems] = useState([]);
+	const [query, setQuery] = useState("");
+	const [searchParam] = useState(["official"]);
+	const [filterParam, setFilterParam] = useState(["All"]);
+
+	useEffect(() => {
+		fetch("https://restcountries.com/v3.1/all")
+			.then((res) => res.json())
+			.then(
+				(result) => {
+					setIsLoaded(true);
+					setItems(result);
+				},
+				// Note: it's important to handle errors here
+				// instead of a catch() block so that we don't swallow
+				// exceptions from actual bugs in components.
+				(error) => {
+					setIsLoaded(true);
+					setError(error);
+				}
+			);
+	}, []);
+
+	const filter = (items) => {
+		return items.filter((item) => {
+			if (item.region == filterParam) {
+				return searchParam.some((newItem) => {
+					return item.name[newItem].toString().toLowerCase().indexOf(query.toLowerCase()) > -1;
+				});
+			} else if (filterParam == "All") {
+				return searchParam.some((newItem) => {
+					return item.name[newItem].toString().toLowerCase().indexOf(query.toLowerCase()) > -1;
+				});
+			}
+		});
+	};
+
+	if (error) {
+		return <div>Error: {error.message}</div>;
+	} else if (!isLoaded) {
+		return <div>Loading...</div>;
+	} else {
+		return (
+			/* here we map over the element and display each item as a card  */
+			<div className='page-wrapper'>
+				<h1 className='main-heading'>Around the World</h1>
+				<div className='filter-wrapper'>
+					<div className='search-wrapper'>
+						<label htmlFor='search-form'>
+							Search countries here:
+							<input
+								type='search'
+								name='search-form'
+								id='search-form'
+								className='search-input'
+								placeholder='Search for...'
+								value={query}
+								onChange={(e) => setQuery(e.target.value)}
+							/>
+						</label>
+					</div>
+					<div>
+						<select
+							/*
+      // here we create a basic select input
+      // we set the value to the selected value
+      // and update the setFilterParam() state every time onChange is called
+      */
+							onChange={(e) => {
+								setFilterParam(e.target.value);
+							}}
+							className='custom-select'
+							aria-label='Filter Countries By Region'>
+							<option value='All'>Filter By Region</option>
+							<option value='Africa'>Africa</option>
+							<option value='Americas'>America</option>
+							<option value='Asia'>Asia</option>
+							<option value='Europe'>Europe</option>
+							<option value='Oceania'>Oceania</option>
+						</select>
+						<span className='focus'></span>
+					</div>
+				</div>
+				<ul className='card-grid'>
+					{filter(items).map((item) => (
+						<li>
+							<article className='card' key={item.name.official}>
+								<div className='card-image'>
+									<img src={item.flags.png} alt={item.name.official} />
+								</div>
+								<div className='card-content'>
+									<h2 className='card-name'>{item.name.official}</h2>
+									<ol className='card-list'>
+										<li>
+											Population: <span>{item.population}</span>
+										</li>
+										<li>
+											Region: <span>{item.region}</span>
+										</li>
+										<li>
+											Capital: <span>{item.capital}</span>
+										</li>
+									</ol>
+								</div>
+							</article>
+						</li>
+					))}
+				</ul>
+			</div>
+		);
+	}
 }
 
 export default App;
+
